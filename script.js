181df37eef89b5aef09b4511036fd19719006213
ronitebul1211 <ronitebul1211@gmail.com>
2020-09-10 21:31:31 +0300

    ADD:logic to get statistic by continent and info type

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('Hi')
 
 /**
  API - Covid 19 -> https://about-corona.net/documentation
@@ -43,3 +42,146 @@ console.log('Hi')
 
  //fetch -> by country -> country statistic
  
+
+ const fetchData = async (url) => {
+  let data;
+  try {
+    const response = await fetch(url);
+    data = response.json()
+  } catch(error) {
+    //throw exception only in network issue
+    console.log(error);
+    console.log('catch');
+  } 
+  return data;
+ };
+
+/**
+ * @param {string} continent - values: Asia / Europe / Americas / Africa 
+ * @return {array} - countries code in ISO 3166-1 alpha-2 format
+ */
+const getCountriesCode = async (continent) => {
+  const proxy = 'https://api.allorigins.win/raw?url=';
+  const baseUrl = 'https://restcountries.herokuapp.com/api/v1/region/'
+  const countriesData = await fetchData(`${proxy}${baseUrl}${continent}`);
+  const countriesCode = [];
+  countriesData.forEach(country => {
+    countriesCode.push(country.cca2);
+  });
+  return countriesCode;
+};
+
+/**
+ * @param {string} - countryCode: in ISO 3166-1 alpha-2 format
+ * @return {object} - data of specific country
+ */
+const getCountryData = async (countryCode) => {
+  const baseUrl = ' https://corona-api.com/countries/'
+  const countryData = await fetchData(`${baseUrl}${countryCode}`);
+  return countryData;
+};
+
+/**
+ * 
+ * @param {string} continent - values: Asia / Europe / Americas / Africa 
+ * @param {string} info - values: deaths / confirmed / recovered / critical
+ * @return {object} statistic - contain labels array & data array to display in chart
+ */
+const getStatistic = async (continent, info) => {
+  const countriesCode = await getCountriesCode(continent);
+  const statistic = {labels: [], data: []};
+  for (const countryCode of countriesCode ) {
+    const country = await getCountryData(countryCode);
+    statistic.labels.push(country.data.name);
+    statistic.data.push(country.data.latest_data[info]);
+  }
+  return statistic;
+}
+
+const handleClick = async() => {
+ const statistic = await getStatistic('Asia', 'deaths');
+ displayInCart(statistic);
+}
+
+const testBtn = document.querySelector('.btn');
+testBtn.addEventListener('click', handleClick);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// var ctx = document.getElementById('chart').getContext('2d');
+// var chart = new Chart(ctx, {
+//     // The type of chart we want to create
+//     type: 'line',
+
+//     // The data for our dataset
+//     data: {
+//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//         datasets: [{
+//             label: 'My First dataset',
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [0, 10, 5, 2, 20, 30, 45]
+//         }]
+//     },
+
+//     // Configuration options go here
+//     options: {}
+// });
+
+const displayInCart = (statistic) => {
+
+  console.log(statistic);
+  
+  var data = {
+    labels: statistic.labels, // Country name arr
+    datasets: [{
+      label: "Dataset #1",
+      backgroundColor: "rgba(255,99,132,0.2)",
+      borderColor: "rgba(255,99,132,1)",
+      borderWidth: 2,
+      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBorderColor: "rgba(255,99,132,1)",
+      data: statistic.data, // num of case arr
+    }]
+  };
+  
+  var options = {
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        stacked: true,
+        gridLines: {
+          display: true,
+          color: "rgba(255,99,132,0.2)"
+        }
+      }],
+      xAxes: [{
+        gridLines: {
+          display: false
+        }
+      }]
+    }
+  };
+  
+  Chart.Bar('chart', {options: options, data: data});
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+//https://restcountries.herokuapp.com/api/v1/region/Asia
+
+// Americas / Asia / Europe / Africa
+// Sub-region: South America / 
